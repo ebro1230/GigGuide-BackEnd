69be2204191581bb72de41ac1d4da59f6a0ac3ca
ebro1230 <ebro1230@gmail.com>
2023-04-27 20:42:24 +0200

    fixing CORS issues for good this time I hope

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const userRouter = require("./router/userRouter.js");
 const cors = require("cors");
 
 app.use(cors("*"));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/user", userRouter);
