64d02def9e6bf1d9fad72d48ef45b63652d8f97e
ebro1230 <ebro1230@gmail.com>
2023-04-27 22:46:11 +0200

    put everything back

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const PORT = process.env.PORT || 8000;
 const bodyParser = require("body-parser");
 const userRouter = require("./router/userRouter.js");
 const artists = require("./router/getLocalArtistsRouter.js");
-const requestEndpoint = `https://cors-anywhere.herokuapp.com/http://ip-api.com/json/?fields=status,message,country,countryCode,city`;
 const cors = require("cors");
-const corsOptions = {
-  origin: "http://localhost:8000/getIP",
-};
 
 app.use(cors("*"));
 app.use(function (req, res, next) {
@@ -31,21 +27,6 @@ app.use("/profile-pics", express.static("./profile-pics"));
 app.use("/banner-pics", express.static("./banner-pics"));
 
 app.use("/api/artists", artists);
-
-app.get("/getIP", cors(), async (req, res) => {
-  const fetchOptions = {
-    method: "GET",
-  };
-  const response = await fetch(requestEndpoint, fetchOptions);
-  if (response) {
-    console.log("Success");
-    console.log(response);
-    const jsonResponse = await response.json();
-    res.json(jsonResponse);
-  } else {
-    console.log(response);
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Hello.  Listening on port ${PORT}`);
